2b24c1bbc3aec8ba1e4ed4945bc7f539e6be2f50
renatocantarino <renato.cantarino@gmail.com>
2025-09-06 15:53:14 -0300

    first commit

---
+++ Car.Api/Program.cs
@@ -0,0 +1,24 @@
+using ServicesDefaults;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
